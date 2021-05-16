16d4644713011ddb0270672756da336a23096560
BuildTools <herwig.matt@gmail.com>
2021-05-15 23:24:09 -0400

    Fleshed out skills section

--- js/index.js
+++ js/index.js
@@ -7,70 +7,30 @@ const quickDetailPanel = $('#quick-detail-panel');
 const navbarClassList = navbar.classList;
 const navbarOriginalOffset = navbar.offsetTop;
 
-window.onload = function () {
-	console.log(`
-		Hey! Gonna be honest, if you are looking at this... 
-		there is much to improve, but hey, this doesn\'t pay
-		the bills... Anyways, thanks for checking out my
-		website!
-	`);
+let skillsChart;
 
-	var ctx = document.getElementById('skills-chart').getContext('2d');
-	var myChart = new Chart(ctx, {
-		type: 'doughnut',
-		data: {
-			labels: ['Java', 'Scala', 'HTML5', 'CSS3', 'JavaScript', 'Python', 'SQL', 'C', 'C++', 'C#', 'Apex', 'Visualforce', 'Aura Components', 'Lightning Web Components'],
-			datasets: [{
-				label: 'Skills',
-				data: [30, 20, 20, 10, 10, 20, 4, 4, 4, 4, 4, 4, 4],
-				borderRadius: 0,
-				backgroundColor: ['#b7aa9f', '#3b4048', '#ab9a93', '#3b4048', '#4d525a', '#dee4ec', '#4b4b55', '#60656e', '#5c5761', '#747981', '#6d636c', '#888d96', '#7e7076'],
-				hoverOffset: 10
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			layout: { padding: 10 },
-			plugins: {
-				legend: { display: false },
-				tooltip: {
-					filter: function (tooltipItem, data) {
-						$('#skills .skill.active').removeClass('active');
-						const languageElement = $('#skills').find(`[data-language='${tooltipItem.label}']`).first();
-						languageElement.addClass('active');
-						return false;
-					},
-				}
-			},
-		}
-	});
+Chart.defaults.defaultFontFamily = 'Montserrat';
+Chart.defaults.defaultFontColor = '#3B4048';
+
+const ChartBackgroundColors = ['#b7aa9f', '#3b4048', '#ab9a93', '#3b4048', '#4d525a', '#dee4ec', '#4b4b55', '#60656e', '#5c5761', '#747981', '#6d636c', '#888d96', '#7e7076'];
+
+/**
+ * ------------------------------------------------------------------------
+ * 				LISTENER METHODS
+ * ------------------------------------------------------------------------
+ */
+
+$(document).ready(function () {
+	printWelcomeMessage()
+	loadSkillsTable();
+	loadSkillsChart();
 	updateStickyNavbar();
-}
+});
 
-window.onscroll = function () {
+$(document).scroll(function () {
 	updateAboutPanel();
 	updateStickyNavbar();
-};
-
-function updateStickyNavbar() {
-	if (window.pageYOffset > navbarOriginalOffset && !navbarClassList.contains(StickyClassName)) {
-		navbarClassList.add(StickyClassName);
-		// This will fill the gap when the navbar sticks to the top of the screen.
-		body.css('margin-top', '57px');
-	} else if (window.pageYOffset < navbarOriginalOffset && navbarClassList.contains(StickyClassName)) {
-		navbarClassList.remove(StickyClassName);
-		body.css('margin-top', '0px');
-	}
-}
-
-function updateAboutPanel() {
-	if (window.innerWidth < 992) return;
-
-	const percentUnadjusted = (((window.pageYOffset - navbarOriginalOffset) / navbarOriginalOffset) * 100) + 110;
-	const percent = Math.max(58, Math.min(100, percentUnadjusted));
-	quickDetailPanel.css('width', percent + '%');
-}
+});
 
 $('#experiences .expander').click(function () {
 	$('#experiences .event-hidden').each(function () { $(this).slideToggle(); });
@@ -85,16 +45,122 @@ $('#education .certification').hover(function () {
 	$(this).find('.detail .back').hide();
 });
 
-$("#skills #skills-chart").mouseleave(function() {
-	$('#skills .skill.active').removeClass('active');
+$('#skills #skills-chart').mouseleave(function() {
+	getActiveSkills().removeClass('active');
+});
+
+$('#skills .nav-link').click(function() {
+	setTimeout(() => updateSkillsChart(), 100);
+});
+
+$('#skills').on('mouseenter', '.skill', function() {
+	$(this).addClass('active');
+	skillsChart.setActiveElements([{ datasetIndex: 0, index: $(this).data('index') }]);
+	skillsChart.render();
+});
+
+$('#skills').on('mouseleave', '.skill', function() {
+	getActiveSkills().removeClass('active');
+	skillsChart.setActiveElements([]);
+	skillsChart.render();
 });
 
 // @note Fixes Bootstrap scrolling issues
-// @link https://stackoverflow.com/questions/49331572/offset-scroll-anchor-in-html-with-bootstrap-4-fixed-navbar/49331692
-var divId;
+// @link https://stackoverflow.com/questions/49331572/offset-scroll-anchor-in-html-with-bootstrap-4-fixed-navbar/49331692;
 $('.navbar .nav-link').click(function(){    
-	divId = $(this).attr('href');
-	$('html, body').animate({
-	scrollTop: $(divId).offset().top - 54
-}, 100);
+	const divId = $(this).attr('href');
+	$('html, body').animate({ scrollTop: $(divId).offset().top - 54 }, 100);
 });
+
+/**
+ * ------------------------------------------------------------------------
+ * 				HELPER METHODS
+ * ------------------------------------------------------------------------
+ */
+
+const printWelcomeMessage = () => console.log(`Thanks for checking out my website!`);
+
+const updateStickyNavbar = () => {
+	if (window.pageYOffset > navbarOriginalOffset && !navbarClassList.contains(StickyClassName)) {
+		navbarClassList.add(StickyClassName);
+		body.css('margin-top', '57px'); // This will fill the gap when the navbar sticks to the top of the screen.
+	} else if (window.pageYOffset < navbarOriginalOffset && navbarClassList.contains(StickyClassName)) {
+		navbarClassList.remove(StickyClassName);
+		body.css('margin-top', '0px');
+	}
+};
+
+const updateAboutPanel = () => {
+	if (window.innerWidth < 992) return;
+
+	const percentUnadjusted = (((window.pageYOffset - navbarOriginalOffset) / navbarOriginalOffset) * 100) + 110;
+	const percent = Math.max(58, Math.min(100, percentUnadjusted));
+	quickDetailPanel.css('width', percent + '%');
+};
+
+const loadSkillsTable = () => {
+	Object.entries(skills).forEach(([category, categorySkills]) => {
+		const totalLevels = categorySkills.reduce((agg, { level }) => agg + level, 0);
+		categorySkills.forEach(({ label, level }, index) => {
+			const percentage = Math.floor((level / totalLevels) * 100);
+			const skillElement = `<span class="skill" data-${category}='${label}' data-percentage='${percentage}' data-index='${index}'>${label}</span>`;
+			$(`#skills .tab-pane[data-category='${category}']`).append(skillElement);
+		});
+	});
+};
+
+const loadSkillsChart = () => {
+	const category = getActiveSkillCategory(),
+				labels = skills[category].map(({ label }) => label),
+				data = skills[category].map(({ level }) => level);
+	
+	const dataset = {
+		data: data,
+		backgroundColor: ChartBackgroundColors,
+		hoverOffset: 15,
+		cutout: '65%'
+	};
+	skillsChart = new Chart($('#skills-chart')[0].getContext('2d'), {
+		type: 'doughnut',
+		data: { labels: labels, datasets: [dataset] },
+		options: {
+			responsive: true,
+			layout: { padding: 15 },
+			animation: { easing: 'easeInOutCubic' },
+			plugins: {
+				legend: { display: false },
+				tooltip: {
+					filter: function (tooltipItem, data) {
+						getActiveSkills().removeClass('active');
+						const categoryElement = $('#skills').find(`[data-${getActiveSkillCategory()}='${tooltipItem.label}']`).first();
+						categoryElement.addClass('active');
+						return false;
+					},
+				},
+				doughnutlabel: {
+					labels: [
+						{ text: getActiveSkillCurrentPercentage, font: { size: '40', family: 'Montserrat-Black' } },
+						{ text: getActiveSkillCurrent, font: { size: '15' } }
+					]
+				}
+			},
+		},
+	});
+};
+
+const updateSkillsChart = () => {
+	const category = getActiveSkillCategory(),
+				categorySkills = skills[category];
+	
+	skillsChart.data.labels = categorySkills.map(({ label }) => label);
+	skillsChart.data.datasets[0].data = categorySkills.map(({ level }) => level);
+	skillsChart.update();
+};
+
+const getActiveSkillCategory = () => $('#skills .nav-link.active').data('category');
+
+const getActiveSkills = () => $('#skills .skill.active');
+
+const getActiveSkillCurrent = () => getActiveSkills().first().data(getActiveSkillCategory()) || '';
+
+const getActiveSkillCurrentPercentage = () => (getActiveSkills().first().data('percentage') || '100') + '%';
